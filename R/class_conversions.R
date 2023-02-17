a90bd0103eaeaa36f6d2aebf9c80bafa87bc3fd9
xavierdidelot <xavier.didelot@gmail.com>
2023-02-17 09:07:27 +0000

    Improved ptreeFromPhyloM and vignette

--- R/class_conversions.R
+++ R/class_conversions.R
@@ -5,17 +5,18 @@
 #' @export
 ptreeFromPhyloM <- function(tr,dateLastSample) {
   ptree=ptreeFromPhylo(p,dateLastSample = dateLastSample)
-  ptree$host=as.numeric(unlist(strsplit(ptree$nam,'\\.'))[seq(1,length(ptree$nam)*2,2)])
-  o=order(ptree$host)
-  o2=o;o2[o]=1:length(o)
-  n=length(ptree$host)
-  ptree$host=ptree$host[o]
-  ptree$nam=ptree$nam[o]
-  ptree$ptree[1:n,]=ptree$ptree[o,]
-  w=which(ptree$ptree[,2]<=n & ptree$ptree[,2]>0)
-  ptree$ptree[w,2]=o2[ptree$ptree[w,2]]
-  w=which(ptree$ptree[,3]<=n & ptree$ptree[,3]>0)
-  ptree$ptree[w,3]=o2[ptree$ptree[w,3]]
+  ptree$host=as.numeric(unlist(strsplit(ptree$nam,'\\.'))[seq(1,length(ptree$nam)*2,2)]) #Extract host information from leaf names
+  n=length(ptree$host) # Number of leaves
+  w=(n+1):nrow(ptree$ptree) # Indexes of internal nodes
+  o=order(ptree$host*1e10+ptree$ptree[1:n,1]) #Order needed for leaves: by host first and then from oldest to most recent
+  o2=order(-ptree$ptree[w,1]) #Order needed for internal nodes: from most recent to oldest (root)
+  o=c(o,n+o2)
+  ot=o;ot[o]=1:length(o)
+  ptree$host=ptree$host[o[1:n]] #Reorder host
+  ptree$nam=ptree$nam[o[1:n]] #Reorder nam
+  ptree$ptree=ptree$ptree[o,] #Reorder ptree
+  ptree$ptree[w,2]=ot[ptree$ptree[w,2]]
+  ptree$ptree[w,3]=ot[ptree$ptree[w,3]]
   class(ptree)<-'ptreem'
   return(ptree)
 }
