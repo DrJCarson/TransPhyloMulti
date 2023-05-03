5c12dd84709456135c920f133c8a79782523be2b
xavierdidelot <xavier.didelot@gmail.com>
2023-05-03 10:36:54 +0100

    Solve warnings

---
+++ R/package.R
@@ -0,0 +1,13 @@
+#' @name TransPhyloMulti
+#' @title TransPhyloMulti: ransPhylo With Multiple Samples Per Host
+#'
+#' @description TransPhyloMulti is a R package extending TransPhylo to allow multiple samples per host.
+#'
+#' @author Jake Carson \email{jake.carson@warwick.ac.uk}
+#'
+#' @import stats
+#' @import graphics
+#' @import ape
+#' @import TransPhylo
+#' @importFrom utils combn getFromNamespace
+NULL
